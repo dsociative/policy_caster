51a24a999fbb538c6a8cb66c076d1a05c58c9908
dsociative <admin@geektech.ru>
2013-10-02 18:39:15 +0400

    added version 1.0

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='policy_caster',
+    version='1.0',
     description='policy caster for flash clients',
     author='dsociative',
     author_email='admin@geektech.ru',
